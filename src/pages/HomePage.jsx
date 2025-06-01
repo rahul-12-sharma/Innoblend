eac2d18585739f84e7c6a4c1c821d0943497376d
Rahul Sharma <rg4142451@gmail.com>
2025-06-01 23:58:35 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Footer from '../components/Footer'
 import HumanAI from '../components/HumanAI'
 import Services from '../components/Services'
 import { HiOutlineMail } from 'react-icons/hi';
-
+import { Link } from 'react-router-dom';
 
 
 function HomePage() {
@@ -13,10 +13,9 @@ function HomePage() {
             <Services />
             <HumanAI />
             <Footer />
-
-            <a
-                href="/contact"
-                className="fixed bottom-25 right-6 flex items-center gap-2 bg-gradient-to-r from-red-500 to-cyan-500 hover:from-red-600 hover:to-cyan-600 text-white px-4 py-2 pr-5 rounded-full shadow-lg hover:scale-105 transition-transform z-50 group"
+            <Link
+                to="/contact"
+                className="fixed bottom-24 right-6 flex items-center gap-2 bg-gradient-to-r from-red-500 to-cyan-500 hover:from-red-600 hover:to-cyan-600 text-white px-4 py-2 pr-5 rounded-full shadow-lg hover:scale-105 transition-transform z-50 group"
                 title="Contact Us"
             >
                 <div className="p-2 bg-white/20 rounded-full">
@@ -25,7 +24,7 @@ function HomePage() {
                 <span className="font-semibold hidden md:inline text-white tracking-wide group-hover:underline">
                     Contact Us
                 </span>
-            </a>
+            </Link>
 
 
         </div>
